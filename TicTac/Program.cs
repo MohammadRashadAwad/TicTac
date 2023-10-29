6f8c19cd88144aa0d90d272cbb0d2ae90993d2d3
Anas Abu Afifeh <a.abuafifeh@certacure.com>
2023-10-29 11:48:41 +0300

    1

--- TicTac/Program.cs
+++ TicTac/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+Console.Write("Enter number : ");
+//int player1 = toInt(Console.ReadLine());
+
