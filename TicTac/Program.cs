4a60c8d650e9880bc79f58d61c71758f87e7524e
m.awad <m.awad@DESKTOP-Q7VIBGA>
2023-10-29 12:49:30 +0300

    init

--- TicTac/Program.cs
+++ TicTac/Program.cs
@@ -1,5 +1,48 @@
-﻿List<int> Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-Console.Write("Enter number : ");
-int player1 = int.Parse(Console.ReadLine());
+﻿class Program
+{
+    static char[] arr = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    static int[] choiceIndex = new int[arr.Length];
+    static void Main(string[] args)
+    {
+        
+        
 
-//aasda
+
+        while (true)
+        {
+            execute(arr);
+        }
+    }
+    static void execute(char[] arr)
+    {
+        Console.WriteLine("Player1:X and Computer:O");
+        Console.Clear();
+        display(arr);
+        Console.WriteLine("Enter choice");
+        var choice = int.Parse(Console.ReadLine());
+        --choice;
+        arr[choice] = 'X';
+
+
+        Console.Clear();
+        display(arr);
+
+    }
+    static void display(char[] arr)
+    {
+        Console.WriteLine("------------------------------");
+        Console.WriteLine($"{arr[0]}\t{arr[1]}\t{arr[2]}");
+        Console.WriteLine("");
+        Console.WriteLine($"{arr[3]}\t{arr[4]}\t{arr[5]}");
+        Console.WriteLine("");
+        Console.WriteLine($"{arr[6]}\t{arr[7]}\t{arr[8]}");
+        Console.WriteLine("------------------------------");
+
+    }
+}
+
+
+
+
+
+
