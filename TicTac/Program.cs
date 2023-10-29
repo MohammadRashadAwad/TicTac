f412355d253dcdeebf92b9aace056f50068b6307
m.awad <m.awad@DESKTOP-Q7VIBGA>
2023-10-29 12:00:23 +0300

    2

--- TicTac/Program.cs
+++ TicTac/Program.cs
@@ -1,4 +1,5 @@
 ﻿List<int> Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 Console.Write("Enter number : ");
-//int player1 = toInt(Console.ReadLine());
+int player1 = int.Parse(Console.ReadLine());
 
+//aasda
